5ac076955027fc1fd96ae23f74134eacdee0bd45
Hassn <adelahmad690@gmail.com>
2016-06-23 22:09:36 +0300

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -97,7 +97,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'❣ عدد الرسائل المرسله : '..user_info_msgs..'\n\n'
-  text = text..'❣ #المطور :@Th3_BOOS\n'
+  text = text..'❣ #المطور :@HASSN_BKS\n'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'id not found.\nuse : /info @username', ok_cb, false)
